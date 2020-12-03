8931763812ca82bd8ee140ae4b9d6a304e4b115f
dixonary <alex@dixonary.co.uk>
2020-12-03 13:36:51 +0000

    Day 2

--- src/Days/Day02.hs
+++ src/Days/Day02.hs
@@ -16,20 +16,45 @@ import Data.Attoparsec.Text
 import Data.Void
 {- ORMOLU_ENABLE -}
 
+import Data.Functor
+import Control.Applicative.Combinators hiding (sepBy)
+import Data.Bits
+
 runDay :: Bool -> String -> IO ()
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  let line = do
+      min <- decimal
+      char '-'
+      max <- decimal
+      char ' '
+      val <- letter
+      string ": "
+      password <- many letter
+      return (min,max,val,password)
+  line `sepBy` endOfLine
+
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [(Int,Int,Char,String)]
 
 ------------ PART A ------------
-partA :: Input -> Void
-partA = error "Not implemented yet!"
+partA :: Input -> Int
+partA = length . filter isValid
+  where 
+    isValid (min,max,val,password) = 
+      let numVal = length $ filter (== val) password
+      in numVal >= min && numVal <= max
+  
 
 ------------ PART B ------------
-partB :: Input -> Void
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB = length . filter isValid
+  where 
+    isValid (a,b,val,password) = 
+      let posA = password !! (a - 1)
+          posB = password !! (b - 1)
+      in (posA == val) `xor` (posB == val)
