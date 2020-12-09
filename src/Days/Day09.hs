e022780f71673f26e87e3725a829dddc4f444f1a
Alex Dixon <alex@dixonary.co.uk>
2020-12-09 16:45:12 +0000

    Day 9

--- src/Days/Day09.hs
+++ src/Days/Day09.hs
@@ -14,6 +14,10 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+
+import Data.Sequence ((<|), Seq, (|>))
+import qualified Data.Sequence as Seq
+import Data.Foldable
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,15 +25,25 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = decimal `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Integer]
 
 ------------ PART A ------------
-partA :: Input -> Void
-partA = error "Not implemented yet!"
+partA :: Input -> Integer
+partA input = let 
+    notInSums :: (Eq a, Num a) => a -> [a] -> Bool
+    notInSums x xs = let xs'  = toList xs 
+                     in x `notElem` [a + b | a <- xs', b <- xs']
 
+    go xs (y:ys) = if y `notInSums` xs then y else go (y : init xs) ys
+    in uncurry go $ splitAt 25 input
+    
 ------------ PART B ------------
-partB :: Input -> Void
-partB = error "Not implemented yet!"
+partB :: Input -> Integer
+partB input = let 
+  a = partA input
+  weakness x = maximum x + minimum x
+  in weakness $ head $ filter (\x -> sum x == a) 
+              $ concatMap inits $ tails input
