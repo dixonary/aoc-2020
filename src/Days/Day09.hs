101f41029e9de0b981821e29b2d87be84e4e7033
Alex Dixon <alex@dixonary.co.uk>
2020-12-09 17:44:24 +0000

    Day 9

--- src/Days/Day09.hs
+++ src/Days/Day09.hs
@@ -2,21 +2,10 @@ module Days.Day09 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
 
-import Data.Sequence ((<|), Seq, (|>))
-import qualified Data.Sequence as Seq
 import Data.Foldable
 {- ORMOLU_ENABLE -}
 
