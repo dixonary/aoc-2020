4f23525f129c1f59fa5c7c58abece6080a4f5847
Alex Dixon <alex@dixonary.co.uk>
2020-12-11 00:33:26 +0000

    Day 10 revisited

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -3,6 +3,7 @@ module Days.Day10 (runDay) where
 {- ORMOLU_DISABLE -}
 import Data.List
 import qualified Data.Vector as Vec
+import qualified Data.Map as Map
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
@@ -26,23 +27,15 @@ type Input = [Int]
 ------------ PART A ------------
 partA :: Input -> Int
 partA ls = let
-  ls' = sort (0:maximum ls + 3:ls)
-  diffs = zipWith (-) (tail ls') ls'
+    ls' = sort (0:maximum ls + 3:ls)
+    diffs = zipWith (-) (tail ls') ls'
   in length (filter (== 1) diffs) * length (filter (== 3) diffs)
 
 ------------ PART B ------------
 partB :: Input -> Integer
-partB ls = runST $ do
-  let top = maximum ls + 3
-  let ls' = sort (top:ls)
-  arr <- Vec.thaw $ Vec.replicate (fromIntegral $ top + 1) 0
-  MVec.write arr 0 1
-  forM_ [1..top] $ \i -> do
-    if i `notElem` ls' 
-    then pure ()
-    else do
-      m2 <- if i < 2 then return 0 else MVec.read arr (i-2)
-      m3 <- if i < 3 then return 0 else MVec.read arr (i-3)
-      m1 <- if i < 1 then return 0 else MVec.read arr (i-1)
-      MVec.write arr i $ m1 + m2 + m3
-  MVec.read arr top
+partB ls = 
+  let 
+    top = maximum ls + 3
+    m !? i = Map.findWithDefault 0 i m
+    go m n = Map.insert n (m !? (n-1) + m !? (n-2) + m !? (n-3)) m
+  in foldl' go (Map.fromList [(0,1)]) (sort $ top:ls) Map.! top
