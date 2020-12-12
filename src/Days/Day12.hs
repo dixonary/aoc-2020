0230eca9281de318fe30ea7877dcbd0b3acc6096
Alex Dixon <alex@dixonary.co.uk>
2020-12-12 14:55:12 +0000

    Day 12

--- src/Days/Day12.hs
+++ src/Days/Day12.hs
@@ -1,19 +1,12 @@
 module Days.Day12 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Data.List ( foldl' )
+import Data.Functor ( ($>) )
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
+import Data.Monoid
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,15 +14,51 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = let
+  movement = choice
+    [ char 'N' $> Dir (0,-1), char 'E' $> Dir (1,0) , char 'S' $> Dir (0,1)
+    , char 'W' $> Dir (-1,0) , char 'L' $> L     , char 'R' $> R
+    , char 'F' $> F
+    ]
+  in ((,) <$> movement <*> decimal) `sepBy1` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [(Movement,Int)]
+
+data Movement = Dir Coord | L | R | F
+  deriving (Show)
+
+type Coord = (Sum Int,Sum Int)
+
+manhattan :: Coord -> Int
+manhattan (a,b) = abs (getSum a) + abs (getSum b)
+
+moveDir :: Coord -> Int -> Coord
+moveDir c d = mconcat $ replicate d c
+
+rot :: Coord -> Int -> Coord
+rot (x,y) amt = case (((fromIntegral amt `div` 90) `mod` 4) + 4) `mod` 4 of
+  0 -> (x,y)
+  1 -> (-y,x)
+  2 -> (-x,-y)
+  3 -> (y,-x)
 
 ------------ PART A ------------
-partA :: Input -> Void
-partA = error "Not implemented yet!"
+partA :: Input -> Int
+partA = manhattan . snd . foldl' move ((1,0), (0,0))
+
+move :: (Coord,Coord) -> (Movement,Int) -> (Coord,Coord)
+move (b,pos) (Dir dr,d) = (b, pos <> moveDir dr d)
+move (b,pos) (L,d)      = (rot b (-d),pos)
+move (b,pos) (R,d)      = (rot b d   ,pos)
+move (b,pos) (F,d)      = (b, pos <> moveDir b d)
 
 ------------ PART B ------------
-partB :: Input -> Void
-partB = error "Not implemented yet!"
+partB :: Input -> Int
+partB = manhattan . snd . foldl' move' ((10,-1),(0,0))
+
+move' :: (Coord,Coord) -> (Movement,Int) -> (Coord,Coord)
+move' (wp,sp) (Dir dr,d) = (wp <> moveDir dr d, sp)
+move' (wp,sp) (L,d)      = (rot wp (-d),sp)
+move' (wp,sp) (R,d)      = (rot wp d   ,sp)
+move' (wp,sp) (F,d)      = (wp, sp <> moveDir wp d)
