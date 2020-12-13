112b4e472d06eb5ee0fe4df7a7800dbc3249f2f1
Alex Dixon <alex@dixonary.co.uk>
2020-12-13 16:43:52 +0000

    Day 13 (broken)

--- src/Days/Day13.hs
+++ src/Days/Day13.hs
@@ -10,10 +10,14 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
+import Data.Functor
 
 import qualified Program.RunDay as R (runDay)
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text hiding (take)
 import Data.Void
+import Control.Applicative
+import Data.Ord
+import Debug.Trace (traceShow, traceShowId)
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,15 +25,30 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do 
+  tot <- decimal
+  endOfLine
+  nums <- ((Just <$> decimal) <|> (char 'x' $> Nothing)) `sepBy1` char ','
+  return (tot,zip [0..] nums)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = (Integer, [(Integer, Maybe Integer)])
 
 ------------ PART A ------------
-partA :: Input -> Void
-partA = error "Not implemented yet!"
+partA :: Input -> Integer
+partA (t,xs) = let 
+  (fx, fr) = minimumBy (comparing snd) $ (\x -> (x,x - t `mod` x)) <$> mapMaybe snd xs
+  in fx * fr
 
 ------------ PART B ------------
-partB :: Input -> Void
-partB = error "Not implemented yet!"
+partB :: Input -> Integer
+partB (_,xs) = let
+  -- Observation: All values we care about are prime (hence coprime).
+  -- This will not be a coincidence.
+  restrict :: [Integer] -> (Integer, Maybe Integer) -> [Integer]
+  restrict ns (_,Nothing) = ns
+  restrict ns (ix,Just x) = 
+      -- Replace our previous infinite list with one restricted to only the ones which are 
+      -- also valid for our new input.
+      let (a:b:_) = take 2 $ filter (\n -> n `mod` x == ix `mod` x) ns in [a,b..]
+  in head $ foldl' restrict [0..] xs
