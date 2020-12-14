eb970f88dafd2bd20ce29befb14ab0affe3c64ff
Alex Dixon <alex@dixonary.co.uk>
2020-12-14 15:51:25 +0000

    Day 14 (broken)

--- src/Days/Day14.hs
+++ src/Days/Day14.hs
@@ -14,6 +14,11 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+import Data.Bits
+import Control.Applicative
+import Data.Functor
+import Control.Monad (foldM)
+import Data.Function
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,14 +26,32 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+  let
+    memset  = do
+      ix <- string "mem[" *> decimal <* string "] = "
+      val <- decimal
+      return $ \(mem,m@(m0,m1)) -> (Map.insert ix ((val .|. m1) .&. m0) mem,m)
+
+    mask = do
+      string "mask = "
+      ls <- many1 $ 
+          char '1' $> Just 1 <|> char '0' $> Just 0 <|> char 'X' $> Nothing
+      let 
+        take1 (Just 1) = 1; take1 _ = 0
+        take0 (Just 0) = 0; take0 _ = 1
+        num = foldl' (\acc x -> acc*2+ x) 0
+      return $ \(mem,_) -> (mem,(num $ take1 <$> ls, num $ take0 <$> ls))
+
+  lines <- (memset <|> mask) `sepBy1` endOfLine
+  return $ fst $ foldl' (&) (Map.empty,(0,0)) lines 
 
 ------------ TYPES ------------
-type Input = Void
+type Input = Map Integer Integer
 
 ------------ PART A ------------
-partA :: Input -> Void
-partA = error "Not implemented yet!"
+partA :: Input -> Integer
+partA = sum . Map.elems
 
 ------------ PART B ------------
 partB :: Input -> Void
