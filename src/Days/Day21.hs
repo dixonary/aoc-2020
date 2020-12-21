86e1f8e04e558c78692303b9ab887ebcff939421
Alex Dixon <alex@dixonary.co.uk>
2020-12-21 18:36:51 +0000

    Day 21

--- src/Days/Day21.hs
+++ src/Days/Day21.hs
@@ -4,16 +4,14 @@ module Days.Day21 (runDay) where
 import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
+import Control.Applicative.Combinators (sepEndBy1)
+import Data.Function ( (&) )
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,15 +19,54 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = let
+    food = do
+      ingredients <- many1 letter `sepEndBy1` char ' '
+      allergens <- do
+        string "(contains "
+        allergens <- many1 letter `sepBy1` string ", "
+        string ")"
+        return allergens
+      return (ingredients, allergens)
+  in food `sepBy1` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Food]
+type Food = ([String], [String])
+
+allergenMap :: [Food] -> Map String String
+allergenMap foods = let
+    allAllergens  = nub $ concat $ snd <$> foods
+
+    -- Find those ingredients which might map to each allergen.
+    candidates :: Map String [String]
+    candidates = allAllergens & U.pairWith ings & Map.fromList
+      where 
+        ings a = foods   
+               & filter ((a `elem`) . snd) -- Those foods with a in allergens
+               & map (Set.fromList . fst)  -- Take their ingredient lists
+               & foldl1' Set.intersection  -- And find the intersection
+               & Set.toList                -- As a list
+
+    computeAllergen cands = 
+      case find (\(_,as) -> length as == 1) $ Map.assocs cands of
+        Nothing    -> Nothing 
+        Just (i,[a]) -> Just ((i,a), cands & Map.delete i & Map.map (delete a))
+
+  in Map.fromList $ unfoldr computeAllergen candidates
 
 ------------ PART A ------------
-partA :: Input -> Void
-partA = error "Not implemented yet!"
+partA :: Input -> Int
+partA foods = let
+    allIngredients = Set.fromList $ concat $ fst <$> foods
+    allergens = allergenMap foods
+    nonAllergens = allIngredients Set.\\ Set.fromList (Map.elems allergens)
+  in length $ concatMap (filter (`Set.member` nonAllergens) . fst) foods
 
 ------------ PART B ------------
-partB :: Input -> Void
-partB = error "Not implemented yet!"
+partB :: Input -> [String]
+partB = Map.elems . allergenMap
+
+-- This is very cursed
+instance {-# OVERLAPS #-} Show [String] where
+  show = intercalate ","
