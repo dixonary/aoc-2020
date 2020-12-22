b6856e14222acaf7bad418cab5375f0eccc827f2
Alex Dixon <alex@dixonary.co.uk>
2020-12-22 15:29:04 +0000

    Day 22: Tidy imports

--- src/Days/Day22.hs
+++ src/Days/Day22.hs
@@ -1,29 +1,20 @@
 module Days.Day22 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
-import Data.Sequence ((|>), (><), Seq((:<|)))
-import qualified Data.Sequence as Seq (take, null, length,  fromList, (|>), Seq((:<|), Empty) )
+import Data.Sequence ((|>), Seq((:<|)))
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Data.Foldable (toList)
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
-import Control.Applicative.Combinators
+import Control.Applicative.Combinators ( sepEndBy1 )
+
 import Control.Monad.State
-import Data.Bool
-import Debug.Trace
-import Control.Monad.Loops
-import Control.Arrow
+import Control.Monad.Loops ( iterateUntilM )
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -88,7 +79,7 @@ recCombat = let
     modify (Set.insert deckStr)
 
     return $ if
-      | seen      -> (uncurry (<>) decks, Seq.Empty) -- Quit on infinite loop
+      | seen      -> (uncurry (<>) decks, mempty) -- Quit on infinite loop
       | win1      -> (d1' |> c1 |> c2, d2')
       | otherwise -> (d1', d2' |> c2 |> c1)
 
