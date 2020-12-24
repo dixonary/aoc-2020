f634f6cd62abd5aa0d03ed62b57296b471ade561
Alex Dixon <alex@dixonary.co.uk>
2020-12-24 14:53:06 +0000

    Tidy imports

--- src/Days/Day24.hs
+++ src/Days/Day24.hs
@@ -2,22 +2,14 @@ module Days.Day24 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, DayRunner)
 import Data.Attoparsec.Text
-import Data.Void
-import Data.Bifunctor
-import Data.Ratio
-import Data.Functor
-import Data.Function
+import Data.Ratio ((%))
+import Data.Functor (($>))
+import Data.Function ((&))
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO (Maybe Double, Maybe Double)
